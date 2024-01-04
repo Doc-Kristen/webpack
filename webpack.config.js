55e35f9fedf766bdecedcbb0fc690b1dfdfe881e
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-01-04 16:31:46 +0300

    feat: реализует обработку html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = (env) => {
+module.exports = env => {
 	return {
 		mode: env.mode ?? 'development',
 		entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -9,5 +11,8 @@ module.exports = (env) => {
 			filename: '[name].[contenthash].js',
 			clean: true,
 		},
+		plugins: [
+			new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+	],
 	}
 }
