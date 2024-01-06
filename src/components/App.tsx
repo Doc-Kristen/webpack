964b922cc391455aa98f60eb95a8b232a988f133
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-01-07 00:04:19 +0300

    feat: реализует работу с scss-файлами

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import './App.scss'
 
 export const App: React.FC = () => {
 	const [count, setCount] = React.useState(0)
 	const increment = () => setCount(prev => prev + 1)
 	return (
-		<>
+		<div className='container'>
 			<div>{count}</div>
-			<button onClick={increment}>+</button>
-			<div>Hello world</div>
-		</>
+			<button className='btn' onClick={increment}>
+				+
+			</button>
+			<h1>Состав сборки:</h1>
+			<ul>
+				<li>Webpack</li>
+				<li>TypeScript</li>
+				<li>React</li>
+				<li>SCSS</li>
+			</ul>
+            <p>Happy coding!</p>
+		</div>
 	)
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -20,11 +20,20 @@ export default (env: EnvVariables) => {
 			filename: '[name].[contenthash].js',
 			clean: true,
 		},
-		plugins: [
-			new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-		],
+		plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
 		module: {
 			rules: [
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						// Creates `style` nodes from JS strings
+						'style-loader',
+						// Translates CSS into CommonJS
+						'css-loader',
+						// Compiles Sass to CSS
+						'sass-loader',
+					],
+				},
 				{
 					test: /\.tsx?$/,
 					use: 'ts-loader',
