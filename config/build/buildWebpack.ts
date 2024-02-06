1998bc652a0a47f0ae6c6cb9b41bc5018a7237e7
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-06 10:23:14 +0300

    service: настраивает source-map

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -21,7 +21,7 @@ export const buildWebpack = (options: BuildOptions): webpack.Configuration => {
 			rules: buildLoaders(options),
 		},
 		resolve: buildResolvers(options),
-		devtool: isDev && 'inline-source-map',
+		devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
 		devServer: isDev ? buildDevServer(options) : undefined,
 	}
 }
